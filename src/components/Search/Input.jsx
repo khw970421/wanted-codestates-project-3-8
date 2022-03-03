2d88be93330a119c571e5a93505a55b2a60b1af9
haagmin5382 <itworker5382@gmail.com>
2022-03-03 16:34:43 +0900

    feat : add debounce

--- src/components/Search/Input.jsx
+++ src/components/Search/Input.jsx
@@ -5,8 +5,21 @@ const Input = ({ changeSearchContent }) => {
     console.log('search Input  : ', target.value);
     changeSearchContent(target.value);
   };
+  const debounce = (callback, delay) => {
+    let timer;
+    return arg => {
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        return callback(arg);
+      }, delay);
+    };
+  };
   return (
-    <input placeholder="검색어를 입력해주세요" onChange={searchSaveTheme} />
+    <input
+      placeholder="검색어를 입력해주세요"
+      onChange={debounce(searchSaveTheme, 400)}
+    />
   );
 };
 Input.propTypes = {
