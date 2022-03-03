0f1ac460d8401c8e780cd4c5ad160daeab6eb63d
jihyeon57 <ehwlgus22@gmail.com>
2022-03-03 16:54:38 +0900

    ✨feat: redux-thunk를 이용한 place 저장

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../redux/modules/reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? composeWithDevTools
+  : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
+
+export default store;
