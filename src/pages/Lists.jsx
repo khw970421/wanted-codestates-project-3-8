f8d1047a3fd7e38359fa28f59e1af5919dddc0d7
sana0803 <sana0803@naver.com>
2022-03-03 19:32:40 +0900

    :sparkles: feat: get api data

--- src/pages/Lists.jsx
+++ src/pages/Lists.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import DataList from '../components/DataList';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Lists = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const res = await axios.get(
+      `/getRecreationalForestInfo?serviceKey=${process.env.REACT_APP_SERVICE_KEY}`,
+    );
+    console.log(res.data.body);
+  };
 
   return (
     <Wrap>
@@ -14,11 +26,9 @@ const Lists = () => {
         <h2>데이터 목록</h2>
       </Nav>
       <ul>
-        <DataList
-          title={'속리산 숲 체험 휴양 마을'}
-          address={'충청북도 보은군 속리산면 속리산로 596'}
-          tel={'043-540-3220'}
-        />
+        {/* {res.data.body.map(({ NM, ADRES, TELNO }, idx) => (
+          <DataList key={idx} title={NM} address={ADRES} tel={TELNO} />
+        ))} */}
       </ul>
     </Wrap>
   );
