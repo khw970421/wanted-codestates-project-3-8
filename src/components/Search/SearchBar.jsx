56ba6b312f4028334501dbd382594b9422cc6623
NamgyungKim <namgyung.kim@gmail.com>
2022-03-03 16:04:03 +0900

    💄 style: SearchBar 컴포넌트 추가, 스타일 적용

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -2,28 +2,33 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Input from './Input';
 import Select from './Select';
-import SaveContents from './SaveContents';
+import styled from 'styled-components';
 
-const Search = ({
+const SearchBar = ({
   options = { name: '이름', address: '주소', memo: '메모' },
 }) => {
   // select option인 searchTheme와 input 검색 결과값인 searchContent 상태 설정
   const [searchTheme, setSearchTheme] = useState('name');
   const [searchContent, setSearchContent] = useState('');
+  console.log(searchTheme, searchContent);
   return (
-    <>
+    <Box>
       <Select
         options={options}
         changeSearchTheme={theme => setSearchTheme(theme)}
       />
       <Input changeSearchContent={content => setSearchContent(content)} />
-      <SaveContents searchTheme={searchTheme} searchContent={searchContent} />
-    </>
+    </Box>
   );
 };
 
-Search.propTypes = {
+const Box = styled.nav`
+  display: flex;
+  margin: 20px;
+`;
+
+SearchBar.propTypes = {
   options: PropTypes.array,
 };
 
-export default Search;
+export default SearchBar;
